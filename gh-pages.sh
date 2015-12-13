cbd7081d3e696c486d068b6fcdd15b15a7b35bee
Thiago Nascimento <thiagodnf@gmail.com>
2015-12-13 11:09:21 -0200

    gh-pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+
+# Create the gh-pages branch first
+#   git checkout --orphan gh-pages
+#   git commit -a -m "first commit"
+#	git push origin gh-pages
+
 git branch -D gh-pages
 git push origin :gh-pages
 
