4fb0cc0cc1bfd671e0c92948b292deff24ef36d0
Thiago Ferreira <thiagodnf@gmail.com>
2023-09-03 20:29:23 -0400

    updated all libraries in the backend

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
     serveStatic = require('serve-static');
-	port = process.env.PORT || 8080;
+	port = process.env.PORT || 3000;
 
 connect().use(serveStatic(__dirname)).listen(port);
 
