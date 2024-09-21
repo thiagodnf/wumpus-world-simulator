e12a9c43625249dec6fb5ac17907cf2f335c1721
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2024-09-21 07:15:51 -0400

    removed nodejs support.

--- app.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-    serveStatic = require('serve-static');
-	port = process.env.PORT || 3000;
-
-connect().use(serveStatic(__dirname)).listen(port);
-
-console.log("Running Wumpus World Simulator");
-console.log('The magic happens at http://localhost:' + port);
